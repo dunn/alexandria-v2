715d3c96bcdc984dee7c94737194054b57cbbef3
Alex Dunn <dunn.alex@gmail.com>
2016-01-20 12:42:29 -0800

    Revert "[test] remove port from tests that don't expect it"
    This reverts commit 5be9a5be3d70119c2e41715f23adfe52fec05b4a.

--- spec/indexers/generic_file_indexer_spec.rb
+++ spec/indexers/generic_file_indexer_spec.rb
@@ -13,7 +13,7 @@ describe GenericFileIndexer do
     subject { indexer.generate_solr_document }
 
     it 'has the url, name and file size' do
-      expect(subject['original_download_url_ss']).to eq 'http://localhost/downloads/bf%2F74%2F27%2F75%2Fbf742775-2a24-46dc-889e-cca03b27b5f3'
+      expect(subject['original_download_url_ss']).to eq 'http://localhost:3000/downloads/bf%2F74%2F27%2F75%2Fbf742775-2a24-46dc-889e-cca03b27b5f3'
       expect(subject['original_filename_ss']).to eq 'sample.pdf'
       expect(subject['original_file_size_ss']).to eq 218_882
     end
